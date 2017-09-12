72bcb7cd9ad52157f82e6a2733606786cb101fea
cjchenliang <chenliang@getcunji.com>
2017-09-12 16:33:50 +0800

    chenliang

--- apps/admin/views/tenmentCatalog/js/add.js
+++ apps/admin/views/tenmentCatalog/js/add.js
@@ -2,7 +2,6 @@ $(function(){
 
     // 验证登录表单
     $("#newHouseCatalogForm").validate({
-        
         focusInvalid: true,
         rules: {
             cid: {
@@ -21,7 +20,53 @@ $(function(){
                     }
                 }
             },
-            
+            title: {
+                required: true
+            },
+            community: {
+                required: true
+            },
+            rent: {
+                required: true,
+                digits: true
+            },
+            show_rent: {
+                required: true
+              
+            },
+            house_type: {
+                required: true
+            },
+            dtype: {
+                required: true
+            },
+        },
+        messages: {
+            cid: {
+                required: "<span style='color:red;'>关联城市名称不能为空 :(</span>",
+                remote: "<span style='color:red;'>没有匹配到该城市，目前仅支持北京下级区县 :(</span>"
+            },
+            title: {
+                required: "<span style='color:red;'>标题不能为空 :(</span>"
+            },
+            community: {
+                required: "<span style='color:red;'>小区不能为空 :(</span>"
+            },
+            rent: {
+                required: "<span style='color:red;'>售价不能为空 :(</span>",
+                digits: "<span style='color:red;'>必须为整数 :(</span>"
+            },
+            show_rent: {
+                required: "<span style='color:red;'>展示价格不能为空 :(</span>"
+                
+            },
+            dtype: {
+                required: "<span style='color:red;'>装修类型不能为空 :(</span>"
+            },
+            house_type: {
+                required: "<span style='color:red;'>房型不能为空 :(</span>"
+            },
+        },
         submitHandler: function(form){
             // 获取htype，ptype选中的值
             var htype = new Array();
@@ -30,7 +75,7 @@ $(function(){
                 htype.push($(this).val());
             });
             // if
-            if (htype != '') {
+            if (htype == '') {
                 swal("拒绝提交", "请勾选出租类型:(", "error");
             } else {
                 $(form).ajaxSubmit({
@@ -61,6 +106,32 @@ $(function(){
     });
 
 });
+
+// 模糊搜索城市名称
+function getCity(){
+    // search
+    var search = $("input[name='search']").val();
+    if (search == false) {
+        sweetAlert("拒绝提交", "城市名称不能为空 :(", "error");
+    } else {
+        // Ajax
+        $.ajax({
+            type: "POST",
+            url: "/admin/city/getCity",
+            data: {search:search},
+            dataType: "JSON",
+            success: function(res){
+                console.log(res);
+            },
+            error: function(e){
+                console.log(e);
+                sweetAlert("未知错误", "请尝试刷新页面后重试 :(", "error");
+            }
+        });
+    }
+
+
+}
 
 
 $(function(){
