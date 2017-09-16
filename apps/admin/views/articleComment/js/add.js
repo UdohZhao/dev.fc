605415169641ba6bda790b6aee14cf552f145181
cjchenliang <chenliang@getcunji.com>
2017-09-16 16:00:57 +0800

    chenliang

--- apps/admin/views/articleComment/js/add.js
+++ apps/admin/views/articleComment/js/add.js
@@ -29,7 +29,7 @@ $(function(){
                         // res
                         if (res === true) {
                             swal("提交成功", "可在此页面继续操作 :)", "success");
-                            window.setTimeout("window.location.reload();",2000);
+                            window.setTimeout("window.location.href=document.referrer;",2000);
                         } else if (res === false) {
                             swal("提交失败", "请尝试刷新页面后重试 :(", "error");
                         } else {
