39c80af5f0c1581f13ab67c32391576d035feb9e
zhaojiangetcunjicom <zhaojian@getcunji.com>
2017-09-18 11:05:49 +0800

    zhaojian

--- core/config/wechat.php
+++ core/config/wechat.php
@@ -3,7 +3,9 @@ return array(
   'APPID'   =>    'wxad31bc3e4960dd3d', // appid
   'APPSECRET'   =>    '20206674b6e4e9edb70000294074ab0a', // appsecret
   'REDIRECT_URI'  =>  isHttps() . '/base/getCode', // 回调地址
-  'UNIT_PERCENT'    =>    '5', // 提成百分比
+  'GENERAL_AGENCY_PERCENT'    =>    '5', // 总代理提成百分比
+  'AGENT_PERCENT'    =>    '10', // 代理商提成百分比
+  'AGENCY_PERCENT'    =>    '15', // 经销商提成百分比
   'CONVERSION'      =>    '100', // 兑换比例，测试为100倍，默认为10倍
-  'PRESENT'         =>    '1000' //新用户首次充值赠送$1000金币
+  'PRESENT'         =>    '100' //新用户赠送$100金币
 );
