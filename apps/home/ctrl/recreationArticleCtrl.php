d1b10fabeaa90f23716ca3cb94bf0f53421559b4
zhaojiangetcunjicom <zhaojian@getcunji.com>
2017-09-20 11:26:03 +0800

    zhaojian

--- apps/home/ctrl/recreationArticleCtrl.php
+++ apps/home/ctrl/recreationArticleCtrl.php
@@ -2,15 +2,18 @@
 namespace apps\home\ctrl;
 use apps\home\model\recreationCategory;
 use apps\home\model\recreationArticle;
+use apps\home\model\belleExtend;
 class recreationArticleCtrl extends baseCtrl{
   public $rcdb;
   public $db;
+  public $bedb;
   public $id;
   public $rcid;
   // 构造方法
   public function _auto(){
     $this->rcdb = new recreationCategory();
     $this->db = new recreationArticle();
+    $this->bedb = new belleExtend();
     $this->id = isset($_GET['id']) ? intval($_GET['id']) : 0;
     $this->rcid = isset($_GET['rcid']) ? intval($_GET['rcid']) : 0;
   }
@@ -19,13 +22,16 @@ class recreationArticleCtrl extends baseCtrl{
    * 休闲娱乐文章详细页面
    */
   public function index(){
-    // Get 
+    // Get
     if (IS_GET === true) {
         // 读取详细信息
         $data = $this->db->getInfo($this->id);
-        // assign 
+        foreach ($data AS $k => $v) {
+          $data[$k]['beData'] = $this->bedb->getCorrelation($v['id']);
+        }
+        // assign
         $this->assign('data',$data);
-        // display 
+        // display
         $this->display('recreationArticle','index.html');
         die;
     }
@@ -35,17 +41,17 @@ class recreationArticleCtrl extends baseCtrl{
    *  读取相关休娱乐数据
    */
   public function indexAll(){
-      // Get 
+      // Get
       if (IS_GET === true) {
         // 读取类别名称
         $cname = $this->rcdb->getCname($this->rcid);
         // 读取相关数据
         $data = $this->db->getAll($this->rcid);
-        // assign 
+        // assign
         $this->assign('cname',$cname);
         $this->assign('data',$data);
-        // display 
-        $this->display('recreationArticle','indexAll.html');        
+        // display
+        $this->display('recreationArticle','indexAll.html');
         die;
       }
   }
